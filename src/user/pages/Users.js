c4acf24201700a35bbfd0d9b754607898b77fb72
ChitownCoder <chitowncoder@gmail.com>
2022-03-12 23:50:15 -0600

    added a userItem component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UserList from '../components/UserList';
+
 const Users = () => {
-  return <h1>Users Works!</h1>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Tim Newman',
+      image:
+        'https://images.pexels.com/photos/2306831/pexels-photo-2306831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      places: 5,
+    },
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
